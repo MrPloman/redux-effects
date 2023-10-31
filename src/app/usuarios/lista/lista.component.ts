100cecbfdac945f466da7b2a2c3c931f5dc50a15
MrPloman <polplanaayet@gmail.com>
2023-10-31 16:59:52 -0500

    All set

--- src/app/usuarios/lista/lista.component.ts
+++ src/app/usuarios/lista/lista.component.ts
@@ -1,17 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { UserService } from '../../services/usuario.service';
 import { User } from '../../models/usuario.model';
 import { AppState } from 'src/app/store/app.state';
 import { Store } from '@ngrx/store';
 import { loadUsers } from 'src/app/store/actions/users.actions';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-lista',
   templateUrl: './lista.component.html',
   styles: [],
 })
-export class ListaComponent implements OnInit {
+export class ListaComponent implements OnInit, OnDestroy {
   usuarios: User[] = [];
+  private usersSubscription: Subscription = new Subscription();
 
   constructor(
     public usuarioService: UserService,
@@ -20,9 +22,15 @@ export class ListaComponent implements OnInit {
 
   ngOnInit() {
     this.store.dispatch(loadUsers());
-    this.store.select('users').subscribe(({ users }) => {
-      if (users) this.usuarios = users;
-      else this.usuarios = [];
-    });
+    this.usersSubscription = this.store
+      .select('users')
+      .subscribe(({ users }) => {
+        if (users) this.usuarios = users;
+        else this.usuarios = [];
+        this.usersSubscription.unsubscribe();
+      });
+  }
+  ngOnDestroy(): void {
+    this.usersSubscription.unsubscribe();
   }
 }
--- src/app/usuarios/usuario/usuario.component.ts
+++ src/app/usuarios/usuario/usuario.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
 import { User } from 'src/app/models/usuario.model';
 import { loadUser } from 'src/app/store/actions/user.actions';
 import { AppState } from 'src/app/store/app.state';
@@ -10,9 +11,10 @@ import { AppState } from 'src/app/store/app.state';
   templateUrl: './usuario.component.html',
   styles: [],
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   public user: User | undefined = undefined;
   public loading: boolean = true;
+  private userSubscription: Subscription = new Subscription();
 
   constructor(private route: ActivatedRoute, private store: Store<AppState>) {}
 
@@ -22,10 +24,15 @@ export class UserComponent implements OnInit {
         this.store.dispatch(loadUser({ id: params.id }));
       }
     });
-    this.store.select('user').subscribe((state) => {
+    this.userSubscription = this.store.select('user').subscribe((state) => {
       this.loading = state.loading;
       if (state.loaded && !state.loading) this.loading = false;
-      if (state && state.user) this.user = state.user;
+      if (state && state.user) {
+        this.user = state.user;
+      }
     });
+  }
+  ngOnDestroy(): void {
+    this.userSubscription.unsubscribe();
   }
 }
